6de4aeaca2b0363332e62d6c896c465e17191926
malcolmberry96 <malcolm.berry65@gmail.com>
2019-03-06 20:59:35 -0600

    starting articles functionality

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from data import Articles
 
 app = Flask(__name__)
+
+Articles = Articles()
 
 
 @app.route('/')
@@ -10,6 +13,10 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_templat('articles.html'. articles = Articles)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,26 @@
+def Articles ():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body' : 'Lorem ',
+            'author' : 'Malcolm Berry',
+            'create_date' : '04-25-19'
+        }, 
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body' : 'Lorem 2 ',
+            'author' : 'Malcolm Berry',
+            'create_date' : '04-25-19'
+        }, 
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body' : 'Lorem ',
+            'author' : 'Malcolm Berry',
+            'create_date' : '04-25-19'
+        }
+    ]
+    
+    return articles
