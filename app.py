3f3f8191ba9755eb18622ed3b5ce6dec20776a2c
malcolmberry96 <malcolm.berry65@gmail.com>
2019-03-08 18:52:34 -0600

    all imports working

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Articles
-from flas_mysqldb import MySQL 
-from wtforms import form, StringField, TextAreaField, PasswordField, validators
+from flask_mysqldb import MySQL 
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
@@ -24,6 +24,23 @@ def articles():
 @app.route('/article/<string:id>/')
 def article(id):
     return render_template('article.html', id=id)
+
+class RegisterForm(Form): 
+    name = StringField ('Name', [validators.Length(min=1, max=50)])
+    username = StringField('Username', [validators.Length(min=4, max=25)])
+    email = StringField('Email', [validators.Length(min=4, max=50)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
+    confirm = PasswordField('Confirm Password')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register ():
+    form = RegisterForm(request.form)
+    if request.method == 'POST' and form.validate():
+        return render_template('register.html')
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
